20add5dc1c18aef6a5f120241af1e2e6099930ea
Israel Ricardo Sestrem <79377858+israel-sestrem@users.noreply.github.com>
2022-03-20 16:17:30 -0300

    Add files via upload

---
+++ Projeto veiculo orientado a objeto/Carro.java
@@ -0,0 +1,40 @@
+package carro;
+
+public class Carro {
+
+    public static void main(String[] args) {
+        
+        Veiculo c = new Veiculo();
+        
+        c.abrirPorta();
+        c.entrarCarro();
+        c.fecharPorta();
+        c.ligarCarro();
+        c.subirMarcha();
+        c.acelerar();
+        c.subirMarcha();
+        c.acelerar();
+        c.subirMarcha();
+        c.acelerar();
+        c.subirMarcha();
+        c.acelerar();
+        c.subirMarcha();
+        c.acelerar();
+        c.descerMarcha();
+        c.desacelerar();
+        c.descerMarcha();
+        c.desacelerar();
+        c.descerMarcha();
+        c.desacelerar();
+        c.descerMarcha();
+        c.desacelerar();
+        c.descerMarcha();
+        c.desacelerar();
+        c.desligarCarro();
+        c.abrirPorta();
+        c.sairCarro();
+        c.fecharPorta();
+        
+    }
+    
+}

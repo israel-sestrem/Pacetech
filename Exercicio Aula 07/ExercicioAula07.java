c00a602b955dd9fe21b51038c46927c71954f303
Israel Ricardo Sestrem <79377858+israel-sestrem@users.noreply.github.com>
2022-03-22 22:02:23 -0300

    Add files via upload

--- Exercicio Aula 07/ExercicioAula07.java
+++ Exercicio Aula 07/ExercicioAula07.java
@@ -1,37 +1,18 @@
 package exercicioaula07;
-import java.util.Scanner;
 import javax.swing.JOptionPane;
 
 public class ExercicioAula07 {
 
     public static void main(String[] args) {
-       
-        String nome, matricula;
-        float notaProva, notaTrabalho;
         
-        Scanner leitor = new Scanner(System.in); 
-        JOptionPane mensagem = new JOptionPane();
-        
-        int i = 2;
-        
-        while ( i > 0 ){          
-            System.out.print("Informe seu nome: ");
-            nome = leitor.next();
-        
-            System.out.print("Informe sua matricula: ");
-            matricula = leitor.next();
-        
-            System.out.print("Informe sua nota da prova: ");
-            notaProva = leitor.nextFloat();
-        
-            System.out.print("Informe sua nota de trabalho: ");
-            notaTrabalho = leitor.nextFloat();
+        for ( int i = 0; i < 2; i++ ){
+            Aluno aluno = new Aluno(
+                    JOptionPane.showInputDialog("Informe seu nome: "),
+                    JOptionPane.showInputDialog("Informe sua matricula: "), 
+                    Float.parseFloat(JOptionPane.showInputDialog("Informe sua nota da prova: ")), 
+                    Float.parseFloat(JOptionPane.showInputDialog("Informe sua nota de trabalho: ")));
             
-            Aluno aluno = new Aluno(nome, matricula, notaProva, notaTrabalho);
-            
-            mensagem.showMessageDialog(null, "Media de " + aluno.getNome() + ": " + aluno.calcularMedia());
-            
-            i--;
+            JOptionPane.showMessageDialog(null, "Media de " + aluno.getNome() + ": " + aluno.calcularMedia());
         }
      
     }
